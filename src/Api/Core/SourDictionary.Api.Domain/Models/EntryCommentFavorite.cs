5da6e2cbe043d9df7aea0b364835b593a531a64a
cihatsolak <cihatsolak@hotmail.com>
2022-04-24 17:40:15 +0300

    Database entity classes added

---
+++ src/Api/Core/SourDictionary.Api.Domain/Models/EntryCommentFavorite.cs
@@ -0,0 +1,11 @@
+﻿namespace SourDictionary.Api.Domain.Models
+{
+    public class EntryCommentFavorite : BaseEntity
+    {
+        public Guid EntryCommentId { get; set; }
+        public Guid CreatedById { get; set; }
+
+        public virtual EntryComment EntryComment { get; set; }
+        public virtual User CreatedBy { get; set; }
+    }
+}
---
+++ src/Api/Core/SourDictionary.Api.Domain/Models/EntryFavorite.cs
@@ -0,0 +1,11 @@
+﻿namespace SourDictionary.Api.Domain.Models
+{
+    public class EntryFavorite : BaseEntity
+    {
+        public Guid EntryId { get; set; }
+        public Guid CreatedById { get; set; }
+
+        public virtual Entry Entry { get; set; }
+        public virtual User CreatedBy { get; set; }
+    }
+}
