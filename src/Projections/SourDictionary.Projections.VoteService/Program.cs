e0a3d580a71edeb9f3bc6222c01d3a234cfe81de
cihatsolak <cihatsolak@hotmail.com>
2022-04-24 17:20:40 +0300

    Project architecture created

---
+++ src/Projections/SourDictionary.Projections.VoteService/Program.cs
@@ -0,0 +1,10 @@
+using SourDictionary.Projections.VoteService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
