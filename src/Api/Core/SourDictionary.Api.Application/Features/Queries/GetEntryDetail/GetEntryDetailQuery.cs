0c9c2cb48eb8d620b85d939d205e96862999e8d8
cihatsolak <cihatsolak@hotmail.com>
2022-05-22 02:30:36 +0300

    Entry details and comments

---
+++ src/Api/Core/SourDictionary.Api.Application/Features/Queries/GetEntryDetail/GetEntryDetailQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace SourDictionary.Api.Application.Features.Queries.GetEntryDetail
+{
+    public class GetEntryDetailQuery : IRequest<GetEntryDetailViewModel>
+    {
+        public Guid EntryId { get; set; }
+        public Guid? UserId { get; set; }
+
+        public GetEntryDetailQuery(Guid entryId, Guid? userId)
+        {
+            EntryId = entryId;
+            UserId = userId;
+        }
+    }
+}
