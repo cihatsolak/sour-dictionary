686ce109f9326921de028b49fe2aec4ec08ffdcd
cihatsolak <cihatsolak@hotmail.com>
2022-05-22 02:34:14 +0300

    Listing user details

---
+++ src/Api/Core/SourDictionary.Api.Application/Features/Queries/GetUserDetail/GetUserDetailQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace SourDictionary.Api.Application.Features.Queries.GetUserDetail
+{
+    public class GetUserDetailQuery : IRequest<UserDetailViewModel>
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+
+        public GetUserDetailQuery(Guid userId, string userName = null)
+        {
+            UserId = userId;
+            UserName = userName;
+        }
+    }
+}
---
+++ src/Api/Core/SourDictionary.Api.Application/Features/Queries/GetUserDetail/GetUserDetailQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace SourDictionary.Api.Application.Features.Queries.GetUserDetail
+{
+    public class GetUserDetailQueryHandler : IRequestHandler<GetUserDetailQuery, UserDetailViewModel>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public GetUserDetailQueryHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UserDetailViewModel> Handle(GetUserDetailQuery request, CancellationToken cancellationToken)
+        {
+            User dbUser = null;
+
+            if (request.UserId != Guid.Empty)
+                dbUser = await _userRepository.GetByIdAsync(request.UserId);
+            else if (!string.IsNullOrEmpty(request.UserName))
+                dbUser = await _userRepository.GetSingleAsync(i => i.UserName == request.UserName);
+
+            // TODO if both are empty, throw new exception
+
+            return _mapper.Map<UserDetailViewModel>(dbUser);
+        }
+    }
+}
