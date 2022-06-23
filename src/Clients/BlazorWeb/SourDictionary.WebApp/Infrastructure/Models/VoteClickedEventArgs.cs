f55824d4819eb0dd8ac73b2654381d60a827b1be
cihatsolak <cihatsolak@hotmail.com>
2022-06-23 23:12:52 +0300

    Entry footer rate favorite component created

---
+++ src/Clients/BlazorWeb/SourDictionary.WebApp/Infrastructure/Models/VoteClickedEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace SourDictionary.WebApp.Infrastructure.Models
+{
+    public class VoteClickedEventArgs : EventArgs
+    {
+        public Guid EntryId { get; set; }
+        public bool IsUpVoteClicked { get; set; }
+        public bool UpVoteDeleted { get; set; }
+        public bool IsDownVoteClicked { get; set; }
+        public bool DownVoteDeleted { get; set; }
+    }
+}
