ad336a1619d270961300b21e48a26f9f856d2836
cihatsolak <cihatsolak@hotmail.com>
2022-07-23 02:32:12 +0300

    Add projection for create entry favorite

---
+++ src/Projections/SourDictionary.Projections.FavoriteService/Services/FavoriteService.cs
@@ -0,0 +1,26 @@
+﻿namespace SourDictionary.Projections.FavoriteService.Services
+{
+    public class FavoriteService
+    {
+        private readonly string _connectionString;
+
+        public FavoriteService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task CreateEntryFavoriteAsync(CreateEntryFavoriteEvent createEntryFavoriteEvent)
+        {
+            using var connection = new SqlConnection(_connectionString);
+
+            await connection
+                .ExecuteAsync("INSERT INTO EntryFavorite (Id, EntryId, CreatedById, CreateDate) VALUES(@Id, @EntryId, @CreatedById, NOW())",
+                new
+                {
+                    Id = Guid.NewGuid(),
+                    createEntryFavoriteEvent.EntryId,
+                    CreatedById = createEntryFavoriteEvent.CreatedBy
+                });
+        }
+    }
+}
--- src/Projections/SourDictionary.Projections.FavoriteService/Worker.cs
+++ src/Projections/SourDictionary.Projections.FavoriteService/Worker.cs
@@ -1,23 +1,33 @@
-using SourDictionary.Common;
-using SourDictionary.Common.Infrastructure;
-
 namespace SourDictionary.Projections.FavoriteService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(
+            ILogger<Worker> logger, 
+            IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            string connectionString = _configuration.GetConnectionString("SourDictionaryDbConnectionString");
+
+            Services.FavoriteService favoriteService = new(connectionString);
+
             QueueFactory.CreateBasicConsumer()
             .EnsureExchange(DictionaryConstants.FavoriteExchangeName)
             .EnsureQueue(DictionaryConstants.CreateEntryFavoriteQueueName, DictionaryConstants.FavoriteExchangeName)
-            .
+            .Receive<CreateEntryFavoriteEvent>(createEntryFavoriteEvent =>
+            {
+                favoriteService.CreateEntryFavoriteAsync(createEntryFavoriteEvent).GetAwaiter().GetResult();
+                _logger.LogInformation("Received EntryId: {id}", createEntryFavoriteEvent.EntryId);
+            })
+            .StartConsuming(DictionaryConstants.CreateEntryFavoriteQueueName);
         }
     }
 }
