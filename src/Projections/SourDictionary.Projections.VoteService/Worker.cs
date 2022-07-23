a36e83d130e91143d6910d05937a9bff9f7e8b31
cihatsolak <cihatsolak@hotmail.com>
2022-07-23 13:24:55 +0300

    Add projection for create vote service

--- src/Projections/SourDictionary.Projections.VoteService/Worker.cs
+++ src/Projections/SourDictionary.Projections.VoteService/Worker.cs
@@ -3,19 +3,31 @@ namespace SourDictionary.Projections.VoteService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(
+            ILogger<Worker> logger, 
+            IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            string connectionString = _configuration.GetConnectionString("SourDictionaryDbConnectionString"); ;
+
+            var voteService = new Services.VoteService(connectionString);
+
+            QueueFactory.CreateBasicConsumer()
+                .EnsureExchange(DictionaryConstants.VoteExchangeName)
+                .EnsureQueue(DictionaryConstants.CreateEntryVoteQueueName, DictionaryConstants.VoteExchangeName)
+                .Receive<CreateEntryVoteEvent>(createEntryVoteEvent =>
+                {
+                    voteService.CreateEntryVoteAsync(createEntryVoteEvent).GetAwaiter().GetResult();
+                    _logger.LogInformation("Create Entry Received EntryId: {0}, VoteType: {1}", createEntryVoteEvent.EntryId, createEntryVoteEvent.VoteType);
+                })
+                .StartConsuming(DictionaryConstants.CreateEntryVoteQueueName);
         }
     }
 }
